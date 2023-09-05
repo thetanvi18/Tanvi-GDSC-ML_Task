d329c37263f00eaa38c71abe18f5a3b0376384f1
Tanvi Dedhia <142569541+thetanvi18@users.noreply.github.com>
2023-09-05 19:09:09 +0530

    Update pyrandompw.py

--- pyrandompw.py
+++ pyrandompw.py
@@ -8,33 +8,17 @@ def generate_password():
     digits = string.digits
     special_characters = '!@#$%^&*_'
 
-    # Create a pool of characters for each category
-    pool_lowercase = random.choice(lowercase_letters)
-    pool_uppercase = random.choice(uppercase_letters)
-    pool_digit = random.choice(digits)
-    pool_special = random.choice(special_characters)
+    lowercase = random.choice(lowercase_letters)
+    uppercase = random.choice(uppercase_letters)
+    digit = random.choice(digits)
+    special = random.choice(special_characters)
 
-    # Combine all character pools
+    # Combine all characters
     all_characters = lowercase_letters + uppercase_letters + digits + special_characters
 
     # Set the password length between 6 and 15 characters
     length = random.randint(6, 15)
-
-    # Calculate the number of characters needed from each pool
-    remaining_length = length - 4  # We've already chosen 4 characters
-    chars_per_pool = [1, 1, 1, remaining_length - 3]  # Subtract 3 for the 3 pools already chosen
-
-    # Randomly select characters from each pool
-    password = random.choice(pool_lowercase) + random.choice(pool_uppercase) + random.choice(pool_digit) + random.choice(pool_special)
-    password += ''.join(random.choice(all_characters) for _ in range(remaining_length))
-
-    # Shuffle the password to make it more random
-    password_list = list(password)
-    random.shuffle(password_list)
-    password = ''.join(password_list)
-
-    return password
-
+    
 # Generate and print the password
 password = generate_password()
 print("Generated Password:", password)
